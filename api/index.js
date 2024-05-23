4dac5e824f8868554cfc64955ff09d71732c8fac
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-05-23 11:51:41 +0200

    update index.js avec le lancement de serveur optimisé

--- api/index.js
+++ api/index.js
@@ -1,56 +1,19 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const booksRoutes = require('./routes/booksRoutes');
 const path = require('path')
-const app = express()
-const mongoose = require('mongoose')
-const { Types: { ObjectId } } = mongoose;
-const port = 80
+const app = express();
+const cors = require('cors')
+const port = 80;
 
 
 // Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
-//---------
-//----------Schemas
-//---------
+app.use(cors());
 
-const bookSchema = new mongoose.Schema({
-    "title": String,
-    "img": String,
-    "author": String,
-    "pages": Number,
-    "category": String,
-    "description": String,
-    "CreatorId": {
-        type: mongoose.Schema.Types.ObjectId
-    }
-  })
-
-  const book = mongoose.model('book', bookSchema, 'Books');
-
-//---------
-//----------Routes
-//---------
-
-//Get all books
-app.get('/books', async (req, res) =>{
-    const books = await book.find();
-    res.json(books)
-});
-
-//Get a book with ID
-app.get('/book/:id', async (req, res) =>{
-    const bookId = req.params.id;
-    const myBook = await book.findById({'_id': bookId});
-    res.json(myBook)
-})
-
-//Delete a book with ID
-app.delete('/book/:id', async (req, res) =>{
-    const bookId = req.params.id;
-    const myBook = await book.findByIdAndDelete({'_id': bookId});
-    res.json('Livre supprimé avec succès, '+myBook+'')
-})
-
+// Utiliser les routes du fichier séparé
+app.use('/', booksRoutes); 
 
 // Connexion à la base de données et lancement du serveur
 const startServer = async () => {
