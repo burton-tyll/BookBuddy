60b17aaf986b3354be07fe28816ceb521442a8f3
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-05-29 16:17:27 +0200

    Ajout de la page connexion et du router + ajout du composant collection

--- web/src/assets/views/Collection.jsx
+++ web/src/assets/views/Collection.jsx
@@ -1,6 +1,27 @@
+import '../style/Collection.css';
+import { Link } from 'react-router-dom';
+
 function Collection(){
     return(
-        <p>test</p>
+        <section className="collection">
+            <div className="menu">
+                <h2>Mes favoris</h2>
+                <div className='links'>
+                    <li><Link to="/mes-collections">Mes collections</Link></li>
+                    <a>Voir tout</a>
+                </div>
+            </div>
+            <div className="collectionContent">
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+                <div className='livreTest'></div>
+            </div>
+        </section>
     )
 }
 
