65e8d13f8807b2d11961f85b27e1dd35c237a78f
Younes Kribeche <younes.kribeche@laplateforme.io>
2024-05-31 11:47:12 +0200

    route pour récup les favoris selon le token de l'utilisateur

--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -11,11 +11,11 @@ router.put('/connexion', userController.connectUser);
 router.get('/users', userController.getUsers);
 router.post('/addUser', userController.addUser);
 
-
 // -----------------------Routes protégées par un token
 router.post('/reward/:parametre', auth, userController.postReward);
 router.get('/users/:id', auth, userController.getUserById);
 router.put('/users/:id', auth, userController.updateUser);
 router.put('/disconnection/:id', auth, userController.disconnectUser);
+router.post('/addFavorite/:bookId', auth, userController.addFavorite); // Nouvelle route
 
 module.exports = router;
